7d28abba300b2c1fba4c56ddfcd2b63c7b61a5fa
nio74 <mpaolo74@gmail.com>
2019-12-17 19:07:15 +0100

    casa1907

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const electron = require('electron')
 const path = require('path')
-const browserWindow = electron.remote.BrowserWindow
+const BrowserWindow = electron.remote.BrowserWindow
 
 const notifyBtn = document.getElementById('notifyBtn')
 
 notifyBtn.addEventListener('click',function(event){
 
-const modalPath = path.join('file://',__dirname,'add.html')
-let win = new browserWindow({width: 400,height: 200 })
-win.on('close',function(){win = null})
-win.loadUrl(modalPath)
-win.show()
+    const modalPath = path.join('file://', __dirname,'add.html')
+    let win = new BrowserWindow({ frame: false, transparent: true, width: 400,height: 200 })
+    win.on('close',function(){win = null})
+    win.loadURL(modalPath)
+    win.show()
 
 })
